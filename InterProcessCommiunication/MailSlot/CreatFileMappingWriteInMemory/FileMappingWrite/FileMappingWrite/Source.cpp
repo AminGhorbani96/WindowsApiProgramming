2da151c66ff48cc4394ad2c718f58acb4093a829
ghorbaniamin96 <ghorbaniamin96@gmail.com>
2023-02-04 08:55:45 -0800

    IPC in Windows MailSlot Shared Memory Read and Write

---
+++ InterProcessCommiunication/MailSlot/CreatFileMappingWriteInMemory/FileMappingWrite/FileMappingWrite/Source.cpp
@@ -0,0 +1,49 @@
+#include <Windows.h>
+#include <stdio.h>
+int main()
+{
+	int shmem_size = 16;
+
+	HANDLE fmhndl = INVALID_HANDLE_VALUE;
+	HANDLE mutex = INVALID_HANDLE_VALUE;
+
+	LPVOID lpMapAddress = NULL;
+	char* data = NULL;
+	
+	mutex = CreateMutex(NULL, FALSE, TEXT("mutex_sample_name"));
+	
+	
+	fmhndl =  CreateFileMapping(INVALID_HANDLE_VALUE,NULL,PAGE_READWRITE,0,shmem_size,TEXT("MemoryFileMap"));
+
+	if (mutex == NULL)
+	{
+		printf("It's Failed to creat MUTEX");
+		return -1;
+	}
+	if (fmhndl == NULL)
+	{
+		printf("It's Failed to creat FileMapping");
+		return -1;
+	}
+
+	lpMapAddress  = MapViewOfFile(fmhndl, FILE_MAP_WRITE, 0, 0, shmem_size);
+
+	data = (char*)lpMapAddress;
+
+	for (int c = 0; c < 60; c++)
+	{
+		WaitForSingleObject(mutex, INFINITE);
+
+		memset(data, c, shmem_size);
+		printf("write shared memory...c=%d\n", c);
+
+		// mutex unlock
+		ReleaseMutex(mutex);
+
+		Sleep(1000);
+	}
+
+	CloseHandle(fmhndl);
+	CloseHandle(mutex);
+	return 0;	
+}
